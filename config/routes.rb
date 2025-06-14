a14d81954d37530aad70d2a91e1c038b2fd05e0e
Thaddaeus Wehner <thad.j.wehner@gmail.com>
2025-06-14 13:25:44 -0500

    WIP(navbar/devise): moving navbar to shared and working on integrating devise with turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get "login", to: "devise/sessions#new", as: :login
+    get "register", to: "devise/registrations#new", as: :register
+    get "logout", to: "devise/sessions#destroy", as: :logout
+  end
   get "up" => "rails/health#show", as: :rails_health_check
   root "landing_page#index", as: :landing
 end
